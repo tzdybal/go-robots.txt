a97e3b14a96dc943d479e975cffc141fde2f1c4c
tzdybal <tomek@zdybal.lap.pl>
2016-11-17 01:34:57 +0100

    robots.txt fetching and parsing (initial version)

--- parser.go
+++ parser.go
@@ -1,5 +1,57 @@
 package robotstxt
 
-func (*RobotsTxt) fetchAndParse(url string) {
+import (
+	"bufio"
+	"net/http"
+	"strings"
+)
 
+func (r *RobotsTxt) fetchAndParse(url string) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return;
+	}
+	defer resp.Body.Close()
+
+	status := resp.StatusCode
+	switch {
+	case 400 <= status && status < 500:
+		// full allow
+		r.sites[url] = &robotsData{map[string][]string{}, map[string][]string{}}
+	case 500 <= status && status < 600:
+		// full disallow
+		r.sites[url] = &robotsData{map[string][]string{"*":{"/"}}, map[string][]string{}}
+	case 200 <= status && status < 300:
+		// conditional allow
+		r.currentUrl = url
+		scanner := bufio.NewScanner(resp.Body)
+		for scanner.Scan() {
+			r.parseLine(scanner.Text())
+		}
+		if err := scanner.Err(); err != nil {
+			// TODO: handle error
+		}
+
+		r.currentUrl = ""
+	}
+
+}
+
+func (r *RobotsTxt) parseLine(line string) {
+	scanner := bufio.NewScanner(strings.NewReader(line))
+	scanner.Split(bufio.ScanWords)
+	scanner.Scan();
+	directive := scanner.Text()
+	scanner.Scan();
+
+	switch directive {
+	case "User-agent:":
+		r.currentAgent = scanner.Text()
+	case "Disallow:":
+		rules := r.sites[r.currentUrl].disallowRules[r.currentAgent];
+		rules = append(rules, scanner.Text())
+	case "Allow:":
+		rules := r.sites[r.currentUrl].allowRules[r.currentAgent];
+		rules = append(rules, scanner.Text())
+	}
 }
